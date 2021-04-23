6e3b252569b295073cfe16be591800a3d9a13c65
Nima Habibkhoda <nima.habibkhoda@ttgint.com>
2021-04-23 16:24:36 +0300

    Change button to reusable component

--- src/components/ChannelManager/BottomActions/BottomActions.component.vue
+++ src/components/ChannelManager/BottomActions/BottomActions.component.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="w-full relative flex flex-row-reverse">
-    <button v-on:click="applyNewObjects" class="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 w-24 rounded-full ml-2">
-      Apply
-    </button>
-    <button v-on:click="restore" class="bg-transparent border border-gray-900 hover:border-gray-400 w-24 text-gray-900 font-bold py-2 px-4 rounded-full">
-      Cancel
-    </button>
+    <Button v-on:handleClick="applyNewObjects" class="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 w-24 rounded-full ml-2" label="Apply"/>
+    <Button v-on:handleClick="restore" class="bg-transparent border border-gray-900 hover:border-gray-400 w-24 text-gray-900 font-bold py-2 px-4 rounded-full" label="Cancel"/>
   </div>
 </template>
 
 <script>
 import {mapMutations} from 'vuex'
+import Button from "../Button/index"
 
 export default {
   name: 'BottomActions',
+  components:{Button},
   methods:{
     ...mapMutations(['restore','applyNewObjects']),
   }
---
+++ src/components/ChannelManager/Button/Button.component.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="clicked" :class="className">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: String,
+    className: String
+  },
+  methods: {
+    clicked() {
+      this.$emit('handleClick');
+    }
+  }
+}
+</script>
