cdf60bcd63ded9d40f6d6b25f58f83e96258b349
Nima Habibkhoda <nima.habibkhoda@ttgint.com>
2021-04-23 16:01:55 +0300

    Fix shadow

--- src/components/ChannelManager/index.vue
+++ src/components/ChannelManager/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" class="min-h-screen p-10 w-full bg-white flex items-center justify-center">
-    <div class="border-2 border-grey p-3 w-2/5 flex flex-col items-center justify-center">
+    <div class="w-2/5 flex flex-col items-center justify-center">
       <span class="text-2xl text-left	w-full font-bold pb-5 pl-5">Channels</span>
-      <div class="shadow border-2 border-grey rounded-2xl p-3 w-full flex-col flex items-center justify-center">
+      <div class="shadow-lg border-2 border-grey rounded-2xl p-3 w-full flex-col flex items-center justify-center">
         <SearchBox/>
         <ChannelList/>
         <BottomActions v-if="showBottom"/>
